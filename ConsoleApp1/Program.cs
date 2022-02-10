6838cf413bf4974081bacab2cb0dc7ce499aa9fd
-JuanLopez- <3014895636juan@gmail.com>
2022-02-10 08:05:29 -0500

    se suben los archivos del proyecto para consola

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp1
+{
+    class Program {
+        static void Main(string[] args)
+        {
+            int opcion;
+            String claveReal = "arroz";
+            string ClaveIngresada = Console.ReadLine();
+            {
+                do
+                {//menu de opciones
+                    if (claveReal == ClaveIngresada) {
+                        Console.WriteLine("-------------");
+                        Console.WriteLine(" Venta de carros");
+                        Console.WriteLine("1. Marcas de carros");
+                        Console.WriteLine("2. Carros ultimo modelo");
+                        Console.WriteLine("3. Carros vendidos");
+                        Console.WriteLine("4. Salir");
+
+                        //leer opcion del usuario
+                        int.TryParse(Console.ReadLine(), out opcion);
+
+                        switch (opcion)
+                        {
+
+                            case 1:
+                                Console.WriteLine("Toyota");
+                                Console.WriteLine("BMW");
+                                Console.WriteLine("Hyundai");
+                                Console.WriteLine("Chevrolet");
+                                break;
+                            case 2:
+                                Console.WriteLine("Hylux");
+                                Console.WriteLine("Onix");
+                                Console.WriteLine("Sedan");
+                                Console.WriteLine("Captiva");
+                                break;
+                            case 3:
+                                Console.WriteLine("Sedan");
+                                Console.WriteLine("Onix");
+                                Console.WriteLine("Captiva");
+                                break;
+                            default:
+                                break;
+                        }
+                        }
+                    } while (opcion != 4) ;
+                }
+
+        }
+
+        } 
+    }
